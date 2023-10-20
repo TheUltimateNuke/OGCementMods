d4c198ed6deb9556e47172fff906097fc6bac6e0
dotpy <41068713+HueSamai@users.noreply.github.com>
2023-10-21 01:49:32 +0200

    Added change punch force

---
+++ ChangePunchForce/ChangePunchForce.cs
@@ -0,0 +1,18 @@
+﻿using CementTools;
+using UnityEngine;
+using Femur;
+
+public class ChangePunchForce : CementMod
+{
+    public void Update()
+    {
+        float multiplier = modFile.GetFloat("PunchForceMultiplier");
+        foreach (Actor actor in FindObjectsOfType<Actor>())
+        {
+            if (actor.ControlledBy == Actor.ControlledTypes.Human)
+            {
+                actor._punchForceModifer = multiplier;
+            }
+        }
+    }
+}
